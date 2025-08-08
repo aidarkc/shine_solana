9a6cb5461ffe30aabc8eba5016953f5b88078cdc
AidarKC <i@aidarkc.ru>
2025-08-08 15:31:23 +0300

    Начал менятьструктуру проекта. Удалил всё лишнее что были обрывки от старых версий

--- programs/shine/src/lib.rs
+++ programs/shine/src/lib.rs
@@ -1,13 +1,5 @@
 use anchor_lang::prelude::*;
 
-mod say_hello;
-
-mod register_user;
-
-
-
-use say_hello::*;
-use register_user::*;
 
 
 mod utils;
@@ -22,35 +14,6 @@ declare_id!("5dFcWDNp42Xn9Vv4oDMJzM4obBJ8hvDuAtPX54fT5L3t");
 #[program]
 pub mod shine {
     use super::*;
-
-    // 
-    // 
-    // /// Тестовая функция — проксирует вызов в модуль `utils`
-    // pub fn test_utils(ctx: Context<TestContext>, extra_pubkey: Pubkey,
-    //     number: u64, note: String,
-    //     str_array: Vec<String>,
-    // ) -> Result<()> {
-    //     test(ctx, extra_pubkey, number, note, str_array)
-    // }
-    // 
-
-    // 
-    // // Вызов функции из say_hello.rs
-    // pub fn say_hello(ctx: Context<SayHello>) -> Result<()> {
-    //     say_hello_impl(ctx)
-    // }
-    // 
-    // // Вызов функции из register_user.rs
-    // pub fn register_user(ctx: Context<RegisterUser>, login: String, pubkey: Pubkey) -> Result<()> {
-    //     register_user_impl(ctx, login, pubkey)
-    // }
-
-    
-    
-    /// Расширенная регистрация пользователя
-    // pub fn do_register_user2(ctx: Context<RegisterUser2>, login: String, pubkey: Pubkey, account_size: u32) -> Result<()> {
-    //     register_user2(ctx.into(), login, pubkey, account_size)
-    // }
 
 
 
